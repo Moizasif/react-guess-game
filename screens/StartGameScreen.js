052b3af8161d9c04f64a970efbb6061d60d4e396
Moizasif <kmoizasif019@gmail.com>
2021-04-02 00:49:48 +0500

    Cleaning UserInput and Controlling the Soft Keyboard

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,23 +1,41 @@
-import React from 'react'
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, Button, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Card from '../components/Card';
 import colors from '../constants/colors';
 import Input from '../components/Input';
 
 
 const StartGameScreen = (props) => {
+    const [enteredValue, setEnteredValue] = useState('');
+
+    const numberInputHandler = inputText => {
+          //First Validation Number Only 0 -9 globally
+          setEnteredValue(inputText.replace(/[^0-9]/g, ''));
+    }
     return (
+        <TouchableWithoutFeedback onPress={() => {
+            Keyboard.dismiss();
+            }}>
         <View style={styles.screen}>
             <Text style={styles.title}>Start a New Game! </Text>
             <Card style={styles.inputContainer}>
                 <Text>Select a Number</Text>
-                <Input style={styles.input} blurOnSubmit autoCapitalize='none' autoCorrect={false} keyboardType="number-pad" maxLength={2} />
+                <Input
+                    style={styles.input}
+                    blurOnSubmit
+                    autoCapitalize='none'
+                    autoCorrect={false}
+                    keyboardType="number-pad"
+                    maxLength={2} 
+                    onChangeText={numberInputHandler}
+                    value={enteredValue}/>
                 <View style={styles.buttonContainer}>
                     <View style={styles.button}><Button title="Reset" onPress={() => { }} color={colors.accent} /></View>
                     <View style={styles.button}><Button title="Confirm" onPress={() => { }} color={colors.primary} /></View>
                 </View>
             </Card>
         </View>
+        </TouchableWithoutFeedback>
 
     )
 }
