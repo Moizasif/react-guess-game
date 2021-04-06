5f382a112d8759f424a739c6aa943c0bc4d11041
Moizasif <kmoizasif019@gmail.com>
2021-04-06 12:06:17 +0500

    Adding Random Num Generator and Switch b/w multiple Screens

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Header from './components/Header';
 import SmartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber);
+  }
+
+  let content = <SmartGameScreen onStartGame={startGameHandler} />;
+  //Check if user selected the Number
+  if (userNumber) {
+    content = <GameScreen userChoice={userNumber}/>
+  }
   return (
     <View style={styles.screen}>
-      <Header title="Guess a Number"/>
-      <SmartGameScreen />
+      <Header title="Guess a Number" />
+      {content}
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +30,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   screen: {
-   flex: 1
+    flex: 1
   },
 });
