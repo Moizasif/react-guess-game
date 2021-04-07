6ee88fe9dbf062f79847ee5864f61a166168c28d
Moizasif <kmoizasif019@gmail.com>
2021-04-07 15:28:00 +0500

    Adding Custom Fonts

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -92,6 +92,9 @@ const styles = StyleSheet.create({
     },
     title: {
         fontSize: 20,
+        marginVertical: 10,
+        fontFamily: 'open-sans-bold' 
+
 
     },
     inputContainer: {
