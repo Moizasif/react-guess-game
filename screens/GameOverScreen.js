d96effabbdeac2b0a69d504e783f0cdd53f8917e
Moizasif <kmoizasif019@gmail.com>
2021-04-08 14:33:51 +0500

    Working with Network Images

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -7,39 +7,40 @@ const GameOverScreen = (props) => {
     return (
         <View style={styles.screen}>
             <TitleText>The Game is Over !</TitleText>
-            <View style = {styles.imageContainer}>
-            <Image 
-            source = {require('../assets/success.png')} 
-            style= {styles.image}
-            resizeMode='cover' />
-            </View> 
+            <View style={styles.imageContainer}>
+                <Image
+                    // source={require('../assets/success.png')}
+                    source={{uri: 'https://blog.strava.com/wp-content/uploads/2018/06/DSC02332-1.jpg'}}
+                    style={styles.image}
+                    resizeMode='cover' />
+            </View>
             <BodyText>Number of Rounds : {props.roundNumber}</BodyText>
             <BodyText>Number was: {props.userNumber}</BodyText>
-            <Button title="NEW GAME" onPress={props.onRestart}/>
+            <Button title="NEW GAME" onPress={props.onRestart} />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-   screen: {
-       flex: 1,
-       justifyContent: 'center',
-       alignItems: 'center'
-   },
-   imageContainer:{
-    width: 300,
-    height: 300,
-       borderRadius: 150,
-       borderWidth: 2,
-       borderColor: 'black',
-       overflow: 'hidden',
-       marginVertical: 30
-   },
-   image: {
-       width: '100%',
-       height: '100%',
-      
-   }
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 2,
+        borderColor: 'black',
+        overflow: 'hidden',
+        marginVertical: 30
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+
+    }
 })
 
 export default GameOverScreen;
