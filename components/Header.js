8a628a4e8ab883ccfc25abf2aa385f44bc073a94
Moizasif <kmoizasif019@gmail.com>
2021-04-10 15:58:56 +0500

    Working with Platform.select and if Checks

--- components/Header.js
+++ components/Header.js
@@ -5,22 +5,28 @@ import colors from '../constants/colors';
 
 const Header = (props) => {
     return (
-        <View style={styles.header}>
+        <View style={{...styles.headerBase, ...Platform.select({ios: styles.headerIOS, android: styles.headerAndroid})}}>
             <TitleText style={styles.title}>{props.title}</TitleText>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    header: {
+    headerBase: {
         width: '100%',
         height: 80,
-        paddingTop: 36,
-        backgroundColor: Platform.OS === 'android' ? colors.primary : 'white',
+        paddingTop: 36,   
         alignItems: 'center',
         justifyContent: 'center',
-        borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
-        borderBottomWidth: Platform.OS === 'ios' ? 1 : 0
+      
+    },
+    headerIOS: {
+        backgroundColor:  'white',
+        borderBottomColor:  '#ccc' ,
+        borderBottomWidth:  1 
+    },
+    headerAndroid: {
+        backgroundColor: colors.primary ,
     },
     title: {
         color: Platform.OS === 'ios' ? colors.primary : 'white'
